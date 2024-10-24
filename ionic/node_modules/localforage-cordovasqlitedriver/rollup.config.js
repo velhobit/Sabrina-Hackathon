f6b3fe87273a551baf6aa39252073879147c4bf4
Rodrigo Portillo <rodrigo@portillodesign.com.br>
2024-10-24 15:57:10 -0300

    Delete ionic/node_modules directory

--- ionic/node_modules/localforage-cordovasqlitedriver/rollup.config.js
+++
@@ -1,8 +0,0 @@
-import babel from 'rollup-plugin-babel';
-import babelrc from 'babelrc-rollup';
-
-export default {
-  entry: 'lib/localforage-cordovasqlitedriver.js',
-  // sourceMap: true,
-  plugins: [babel(babelrc())]
-};
